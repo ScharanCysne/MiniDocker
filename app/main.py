68c316c2219d0ef9dde48994b193a30cd0f5044d
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-06-04 16:59:14 -0300

    Isolating command by moving to temp directory

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
+import os
 import subprocess
 import sys
+
+from tempfile import mkdtemp
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
+    temp_dir = mkdtemp()
+    os.chroot(temp_dir)
 
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stderr.write(completed_process.stderr.decode("utf-8"))
